6ba3ed4806af13f9af7ab985f449014f8d9a1135
Mrityunjay <mrityunjaybhardwaj99@gmail.com>
2021-07-21 17:32:41 +0530

    split page-content into 3

--- assets/output/bootstrap.js
+++ assets/output/bootstrap.js
@@ -87,6 +87,6 @@ void (async function bootstrap() {
 
   // Now that all federated containers are initialized with the main
   // container, we can import the main function.
-  let main = (await import('./index.js')).main;
+  let main = (await import('./index.js.js.js')).main;
   window.addEventListener('load', main);
 })();
